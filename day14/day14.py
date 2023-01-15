57c5e7f473bf4d984fb9c50e6aa1f1536355a0a5
DiogoAlves002 <diogoasilva@ua.pt>
2023-01-15 20:27:03 +0000

    day 14 done (its really inefficient, specially part 2, but it works)

--- day14/day14.py
+++ day14/day14.py
@@ -1,6 +1,4 @@
 
-import time
-
 
 def store_rocks(lines):
     """return the grid height and a list of tuples, each tuple is a line of rocks, and contains tuples of (x, y) points that make that line"""
@@ -15,13 +13,13 @@ def store_rocks(lines):
             if int(y) > bottom:
                 bottom= int(y)
         rocks.append(t)
-    #print(rocks)
+    bottom += 2
     return bottom, rocks
 
 
 def read_data():
-    with open("day14/test_input.txt", "r") as f:
-    #with open("day14/input.txt", "r") as f:
+    #with open("day14/test_input.txt", "r") as f:
+    with open("day14/input.txt", "r") as f:
 
         lines= f.read().splitlines()
 
@@ -65,11 +63,11 @@ def match_vertical_rock(sand_Y, start, finish):
     return False
 
 
-def match_sand(sand_X, sitting_sand):
+def match_sand(sand_X, sand_Y, sitting_sand):
     top= None
     for sand in sitting_sand:
-        if sand[0] == sand_X:
-            if top is None or sand[1] < top:
+        if sand[0] == sand_X and sand[1] >= sand_Y: # ignore sand above the current drop point
+            if top is None or sand[1] < top: # top of the pile
                 top= sand[1]
     return top
 
@@ -85,38 +83,54 @@ def get_rock_top(start, finish):
         return finish_Y
 
 
+def blocked_by_rock(sand, rocks):
+    sand_X, sand_Y= sand
+    for rock in rocks:
+        start= rock[0]
+        for i in range(1, len(rock)):
+            finish= rock[i]
+            if match_horizontal_rock(sand_X, start, finish) and match_vertical_rock(sand_Y, start, finish):
+                return True # blocked by a rock
+            start= finish
+    return False
+
 def slide_sand(sand, bottom, rocks, sitting_sand, direcion):
-    top = None
-    if direcion == "left":
-        sand_X, sand_Y= sand
-        sand_X -= 1
-        sand_Y += 1
-        for rock in rocks:
-            start= rock[0]
-            for i in range(1, len(rock)):
-                finish= rock[i]
-                #print("start: ", start, "finish: ", finish)
-                if match_horizontal_rock(sand_X, start, finish):
-                    if top is None or (top > get_rock_top(start, finish)-1 and get_rock_top(start, finish)-1 > sand_Y):
-                        top= get_rock_top(start, finish)-1
-                start= finish
-
-        sand_top= match_sand(sand_X, sitting_sand)
-        if sand_top is not None:
-            if top is None or top > sand_top- 1:
-                top= sand_top- 1
-        
-        if top is None: # sand has fallen off to the void
-            print("fallen off while sliding", sand_X, top)
-            return None       
-
-
-    else:
-        sand_X, sand_Y= sand
-        sand_X += 1
+    sand_X, sand_Y= sand
+    sand_Y += 1
+    sand_X += 1 if direcion == "right" else -1
     
+    if blocked_by_rock((sand_X, sand_Y), rocks):
+        return -1
+
+    sand_top= match_sand(sand_X, sand_Y, sitting_sand)
+    if sand_top is not None and sand_top == sand_Y:
+        return -1 # blocked by sand
+
     return drop_sand((sand_X, sand_Y), bottom, rocks, sitting_sand)
 
+
+def update_bottom(rocks, sand_X, bottom):
+    rock_bottom= rocks[-1]
+
+    current_bottom= 0
+    for point in rock_bottom:
+        if point[1] > current_bottom:
+            current_bottom= point[1]
+
+    if current_bottom != bottom: # bottom has not been added yet
+        rocks.append(((sand_X-2, bottom), (sand_X+2, bottom)))
+        return rocks
+        
+    rock_bottom_start, rock_bottom_finish= rock_bottom
+    if rock_bottom_start[0] >= sand_X:
+        rock_bottom_start= (sand_X-2, bottom)
+    else:
+        rock_bottom_finish= (sand_X+2, bottom)
+    
+    rocks[-1]= (rock_bottom_start, rock_bottom_finish)
+    return rocks
+
+    
 
 
 def drop_sand(sand, bottom, rocks, sitting_sand):
@@ -127,34 +141,40 @@ def drop_sand(sand, bottom, rocks, sitting_sand):
         start= rock[0]
         for i in range(1, len(rock)):
             finish= rock[i]
-            #print("start: ", start, "finish: ", finish)
             if match_horizontal_rock(sand_X, start, finish):
-                if top is None or (top > get_rock_top(start, finish)-1 and get_rock_top(start, finish)-1 > sand_Y):
-                    top= get_rock_top(start, finish)-1
+                rock_top= get_rock_top(start, finish)
+                if rock_top-1 >= sand_Y and(top is None or top > rock_top-1):
+                    top= rock_top-1
             start= finish
-    #print("top rock", top)
-    sand_top= match_sand(sand_X, sitting_sand)
+            
+    sand_top= match_sand(sand_X, sand_Y, sitting_sand)
     if sand_top is not None:
+        if sand_top == 0: # blocked the entrance
+            return None
         if top is None or top > sand_top- 1:
             top= sand_top- 1
 
 
-    fallen_sand= (sand_X, top) # sand has fallen to the top of the rock or sand
+    if top is None: # sand has fallen off to the void
+        if bottom is None: # challenge 1
+            return None
 
+        rocks= update_bottom(rocks, sand_X, bottom)
+        top = bottom-1
+
+    
+    fallen_sand= (sand_X, top) # sand has fallen to the top of a rock or sand
+    
     # now we need to check if the sand can slide to the left or right
     slide_left= slide_sand(fallen_sand, bottom, rocks, sitting_sand, "left")
-    if slide_left:
+    if slide_left != -1:
         return slide_left
     
-    #slide_right= slide_sand(fallen_sand, bottom, rocks, sitting_sand, "right")
-    #if slide_right:
-    #    return slide_right
+    if slide_left == -1: # sand is blocked by a rock or sand
+        slide_right= slide_sand(fallen_sand, bottom, rocks, sitting_sand, "right")
+        if slide_right != -1:
+            return slide_right
 
-    if top is None: # sand has fallen off to the void
-        print("fallen off", sand_X, top)
-        return None
-
-    #print("fallen sand", sand_X, top)
     return fallen_sand
 
 
@@ -164,78 +184,79 @@ def print_rock(rocks, x, y):
         for i in range(1, len(rock)):
             finish= rock[i]
             if match_horizontal_rock(x, start, finish) and match_vertical_rock(y, start, finish):
-                print("#", end="")
-                return False
+                return "#"
             start= finish
-    return True
+    return None
 
 def print_sand(sitting_sand, x, y):
     for sand in sitting_sand: # print sand
         if sand[0] == x and sand[1] == y:
-            print("o", end="")
-            return False
-    return True
+            return "o"
+    return None
 
 def draw_grid(rocks, sitting_sand, dimensions= ((494, 0), (503, 9)), drop= (500, 0)):
 
-    print("")
+    grid= ['\n']
     for i in range(3): # print grid labels
         s= str(dimensions[0][0])[i]
         f= str(dimensions[1][0])[i]
         d= str(drop[0])[i]
         if dimensions[1][1] + 1 > 9:
-            print(" ", end="")
+            grid.append(" ")
             if dimensions[1][1] + 1 > 99:
-                print(" ", end="")
-        print("  "+ s + " "*(drop[0] - dimensions[0][0] - 1) + d + " "* (dimensions[1][0] - drop[0] - 1) + f)
+                grid.append(" ")
+        #grid.append(" ") # needed for test input but not for challenge input (dunno why but tbh dont really care)
+        grid.append("  "+ s + " "*(drop[0] - dimensions[0][0] - 1) + d + " "* (dimensions[1][0] - drop[0] - 1) + f + "\n")
 
     for y in range(dimensions[0][1], dimensions[1][1] + 1):
-        print(y, end=" ")
+        grid.append(str(y)+ " ")
         if y < 10:
-            print(" ", end="")
+            grid.append(" ")
         if y < 100:
-            print(" ", end="")
+            grid.append(" ")
         for x in range(dimensions[0][0], dimensions[1][0] + 1):
 
-            if (x, y) == drop: # print drop
-                print("+", end="")
+            if (x, y) == drop:
+                grid.append("+")
                 continue
 
-            no_rock= print_rock(rocks, x, y) # print rocks
-            
-            if no_rock: # print sand
-                no_sand= print_sand(sitting_sand, x, y)
-
-            if no_rock and no_sand:
-                print(".", end="")
-        print("")
+            rock= print_rock(rocks, x, y)
+            if rock:
+                grid.append(rock)
+                continue
 
 
-def challenge_1(bottom, rocks, start= (500, 0)):
+            sand= print_sand(sitting_sand, x, y)
+            if sand:
+                grid.append(sand)
+                continue
+
+            grid.append(".")
+        grid.append("\n")
+
+    print("".join(grid))
+
+
+def challenge(rocks, start, bottom= None):
     units= 0
-    sitting_sand= []
+    sitting_sand= set()
 
-    dimensions= ((494, 0), (503, 9)) # for test input
-    #dimensions=((400, 0), (600, 200)) # for final input
+    #dimensions= ((488, 0), (508, 11)) # for test input
+    dimensions=((300, 0), (700, 200)) # for final input
     while True:
         sand= drop_sand(start, bottom, rocks, sitting_sand)
-        if sand is None: # fell to the void
+        if sand is None: # fell to the void or blocked the entrance
             break
-        sitting_sand.append(sand)
+        sitting_sand.add(sand)
         
-        draw_grid(rocks, sitting_sand, dimensions, drop= start)
-
         units+= 1
-        time.sleep(1)
 
-
-
+        if units % 100 == 0:
+            print(units)
+    draw_grid(rocks, sitting_sand, dimensions, drop= start) # final grid
     return units
     
 
-
-def challenge_2(all_packets): 
-    pass
 
 
 
@@ -243,12 +264,15 @@ def challenge_2(all_packets):
 def main():
 
     bottom, rocks= read_data()
+    start= (500, 0)
 
-    units= challenge_1(bottom, rocks)
+    units= challenge(rocks, start)
+    units_2 = challenge(rocks, start, bottom)
+
 
     print("Challenge 1: ", units)
+    print("Challenge 2: ", units_2)
 
-    #draw_grid(rocks, [(500, 8), (500, 7), (499, 8)])
 
 
 
